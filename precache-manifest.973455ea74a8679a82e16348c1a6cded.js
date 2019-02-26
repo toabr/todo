3c1fa7775d85075c3127f57f255c08702770a251
c3to <c3to@web.de>
2019-02-26 19:46:48 +0100

    Updates

---
+++ precache-manifest.973455ea74a8679a82e16348c1a6cded.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b16784f5aabc2900b92",
+    "url": "/todo/static/js/runtime~main.3b16784f.js"
+  },
+  {
+    "revision": "7b503fc6a73b7ffbf221",
+    "url": "/todo/static/js/main.7b503fc6.chunk.js"
+  },
+  {
+    "revision": "9da6ba8f352bb39e610a",
+    "url": "/todo/static/js/2.9da6ba8f.chunk.js"
+  },
+  {
+    "revision": "7b503fc6a73b7ffbf221",
+    "url": "/todo/static/css/main.5411110f.chunk.css"
+  },
+  {
+    "revision": "ddec48a345aabe898edf48712eb448fe",
+    "url": "/todo/index.html"
+  }
+];
